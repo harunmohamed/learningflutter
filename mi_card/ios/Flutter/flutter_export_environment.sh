e908956cb5938575230018eabd2c454a8b37fb99
harunmohamed <harunmohamed901@gmail.com>
2021-02-16 15:59:26 +0200

    Mi Card

---
+++ mi_card/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/harun/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/harun/Desktop/flutter-bootcamp/mi_card"
+export "FLUTTER_TARGET=/Users/harun/Desktop/flutter-bootcamp/mi_card/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/harun/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
